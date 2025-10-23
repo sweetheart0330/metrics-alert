134be2ddb65918e2fea7f1c53d3ec1c2c937057b
sweetheart0330 <sweetschoollorden@gmail.com>
2025-10-24 00:17:19 +0400

    fixed mock

--- internal/mocks/mock_client.go
+++ internal/mocks/mock_client.go
@@ -14,6 +14,7 @@ import (
 
 	models "github.com/sweetheart0330/metrics-alert/internal/model"
 	gomock "go.uber.org/mock/gomock"
+	zap "go.uber.org/zap"
 )
 
 // MockIClient is a mock of IClient interface.
@@ -69,15 +70,15 @@ func (mr *MockIClientMockRecorder) SendGaugeMetric(m any) *gomock.Call {
 }
 
 // SendMetricsBatch mocks base method.
-func (m *MockIClient) SendMetricsBatch(metrics []models.Metrics) error {
+func (m *MockIClient) SendMetricsBatch(metrics []models.Metrics, log *zap.SugaredLogger) error {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "SendMetricsBatch", metrics)
+	ret := m.ctrl.Call(m, "SendMetricsBatch", metrics, log)
 	ret0, _ := ret[0].(error)
 	return ret0
 }
 
 // SendMetricsBatch indicates an expected call of SendMetricsBatch.
-func (mr *MockIClientMockRecorder) SendMetricsBatch(metrics any) *gomock.Call {
+func (mr *MockIClientMockRecorder) SendMetricsBatch(metrics, log any) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SendMetricsBatch", reflect.TypeOf((*MockIClient)(nil).SendMetricsBatch), metrics)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "SendMetricsBatch", reflect.TypeOf((*MockIClient)(nil).SendMetricsBatch), metrics, log)
 }
